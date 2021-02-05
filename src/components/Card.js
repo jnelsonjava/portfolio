bb18dccb6b9deec29d90989e052a9d7ef6ee187a
Jack Nelson <jnelson.java@gmail.com>
2021-02-04 23:18:51 -0800

    implementing card info

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,8 @@ import CardInfo from '../components/CardInfo';
 function Card(props) {
 
   return (
-    <div>
-      <img className="" src={props.item.imgSrc} alt={props.item.imgSrc} />
+    <div className="d-inline-block p-card" onClick={(e) => props.click(props.item)}>
+      <img className="p-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
       { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} /> }
     </div>
   );
--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
 
 function CardInfo(props) {
  
+  const style = useSpring({opacity: 1, from: {opacity: 0}});
 
   return(
-    <p>this is card info</p>
+    <animated.div className="p-card-info" style={style}>
+      <p className="p-card-title">{props.title}</p>
+      <p className="p-card-sub-title">{props.subTitle}</p>
+      <a href={props.link} target="_blank" rel="noopener noreferrer">{props.link}</a>
+    </animated.div>
   );
 }
 
