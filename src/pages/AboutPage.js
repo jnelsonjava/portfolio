963f10ec018fdc19803ba806166c9368b7503a64
Jack Nelson <jnelson.java@gmail.com>
2021-02-05 23:15:10 -0800

    added content to about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Content from '../components/Content';
+import Hero from '../components/Hero'
 
 function AboutPage(props) {
+
   return (
-    <p>This is the About Page!</p>
+    <div>
+      <Hero title={props.title} />
+
+      <Content>
+        <p>Hi! I'm Jack. I do Java. This is some placeholder text though.</p>
+      </Content>
+    </div>
   );
+
 }
 
 export default AboutPage;
